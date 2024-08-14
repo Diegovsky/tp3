c5ad8f04771ccc309d9d33ae5464bac492993587
Diego Augusto S. C <diego.augusto@protonmail.com>
2024-08-14 16:07:09 -0300

    Add brute force
    Co-Authored-By: Lucas Leite @lucaselc

--- src/brute.c
+++ src/brute.c
@@ -1,5 +1,26 @@
 #include "brute.h"
+#include <stdio.h>
+#include <string.h>
 
 bool brute_strstr(char* haystack, char* needle) {
+    int i, j, k;
+    int haystack_len = strlen(haystack);
+    int needle_len = strlen(needle);
+
+
+    for(i = 0; i <= (haystack_len - needle_len); i++) {
+        k = i;
+        j = 0;
+
+        while(haystack[k] == needle[j] && j < needle_len) {
+            j++;
+            k++;
+        }
+
+
+        if(j == needle_len) {
+            return true;
+        }
+    }
     return false;
 }
